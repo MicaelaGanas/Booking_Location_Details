bfd5a607826bf603211467b8fcaab90e9e3f66f6
MicaelaGanas <myganas@mcm.edu.ph>
2024-05-21 18:49:33 +0800

    salot gyud sya sa kinabuhi

--- app/src/main/java/com/example/location_view_booking/expand_text_view.java
+++ app/src/main/java/com/example/location_view_booking/expand_text_view.java
@@ -1,5 +1,6 @@
 package com.example.location_view_booking;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,47 +8,46 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class expand_text_view extends AppCompatActivity {
 
-    TextView text1, text2;
     @Override
-    protected void onCreate(Bundle savedInstranceState) {
-        super.onCreate(savedInstranceState);
-        setContentView(R.layout.expand_text_view);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        text1 = findViewById(R.id.text1);
-        text2 = findViewById(R.id.text2);
+        TextView text1 = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.text2);
 
-        ImageView button1 = findViewById(R.id.button1);
-        ImageView button2 = findViewById(R.id.button2);
+        ImageView button1 = findViewById(R.id.btn1);
+        ImageView button2 = findViewById(R.id.btn2);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (text1.getVisibility() -- View.GONE) {
+            public void onClick(View v) {
+                if (text1.getVisibility() == View.GONE) {
                     text1.setVisibility(View.VISIBLE);
                     button1.setImageResource(R.drawable.baseline_keyboard_arrow_up_24);
                 } else {
                     text1.setVisibility(View.GONE);
                     button1.setImageResource(R.drawable.baseline_keyboard_arrow_down_24);
+
                 }
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (text2.getVisibility() -- View.GONE) {
+            public void onClick(View v) {
+                if (text2.getVisibility() == View.GONE) {
                     text2.setVisibility(View.VISIBLE);
                     button2.setImageResource(R.drawable.baseline_keyboard_arrow_up_24);
                 } else {
                     text2.setVisibility(View.GONE);
                     button2.setImageResource(R.drawable.baseline_keyboard_arrow_down_24);
+
                 }
             }
         });
-
 
     }
 }
